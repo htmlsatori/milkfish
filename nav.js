3d7969d310bcf69708747f004ba7669dbe9b8420
htmlsatori <satorijal@gmail.com>
2023-04-27 17:30:04 -0400

    FINAL

--- nav.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.nav__toggle').on('click', function() {
-      $('.nav').toggleClass('display');
-    });
-    $('.nav__toggle').on('click', function() {
-      $('.menu__item').toggleClass('display');
-    });
-  });
